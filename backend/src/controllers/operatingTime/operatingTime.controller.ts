356db678d5a0baf14b3b9c2afe945ca58ca4eb94
Pedro Ribeiro <86488494+pedroheribeiro2021@users.noreply.github.com>
2023-05-18 16:28:39 -0300

    Revert "Feat home page"

--- backend/src/controllers/operatingTime/operatingTime.controller.ts
+++
@@ -1,9 +0,0 @@
-// import { Request, Response } from "express"
-// import { createRestaurantService } from "../../services/restaurants/createRestaurants.service";
-
-// export const createRestaurantsController = async (req: Request, res: Response) => {
-//     const restaurantData = req.body
-//     const operatingTimeData = req.body.operatingTime
-//     const body = await createRestaurantService(restaurantData, operatingTimeData)
-//     return res.status(201).json(body)
-// }
--- backend/src/routes/isOpen.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { isOpenController } from "../controllers/restaurants/restaurants.controller";
-
-
-export const isOpenRoutes = Router()
-
-isOpenRoutes.post('/', isOpenController)
