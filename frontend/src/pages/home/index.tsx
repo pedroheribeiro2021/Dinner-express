e09d0736bcfcd93e204f7c1627d7dc630a348cfc
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-05-19 14:35:11 -0300

    fixing errors

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+import cover from "../../assets/dinner wallpapaer.jpg"
+import { Header } from "../../components/header"
+import { useRestaurantContext } from "../../contexts/restaurantsContext"
+import { HomeStyle } from "./style"
+import { ModalCreateRestaurant } from "../../components/modal create restaurant"
+import React from "react"
+
+export const Home = () => {
+    const { getAllRestaurants, restaurants } = useRestaurantContext()
+
+    const [expandedRestaurant, setExpandedRestaurant] = useState(null)
+    
+    useEffect(() => {
+        getAllRestaurants()
+      }, [])
+
+    return (
+        <>
+        <Header/>
+        <HomeStyle>
+            <ModalCreateRestaurant/>
+            <h1>LUGARES INCRIVEIS!</h1>
+            <h3 className="subtitle1">Seja bem vindo ao Food Express!
+                Seu portal de restaurantes mais completo da internet.
+            </h3>
+            <h2>DINNER EXPRESS</h2>
+            <h3 className="subtitle2">Lanchonetes, restaurantes, sorveterias e muito mais para matar sua fome.</h3>
+            <div className="restaurants-hub">
+                <img className="cover" src={cover} alt=""/>
+                <ul className="restaurants-list">
+                    {
+                        restaurants.length > 0 ? restaurants.map((restaurant: any, i: number) => (
+                            <li key={i}>
+                                {expandedRestaurant !== restaurant && (
+                                    <>
+                                        <h4>{restaurant.name}</h4>
+                                        <p>{restaurant.type}</p>
+                                    </>
+                                )}
+                                {
+                                    expandedRestaurant === restaurant && (
+                                        <>
+                                        <h5>Horário de funcionamento:</h5>
+                                          <ul className="operating-time">
+                                            <button onClick={() => setExpandedRestaurant(null)}>Voltar</button>
+                                            {restaurant.operatingTimes.map((times: any, i: number) => (
+                                                <li key={times.dayOfWeek}>
+                                                {times.dayOfWeek} ⋅ De {times.openingTime} ⋅ Às {times.closingTime}
+                                              </li>
+                                            ))}
+                                          </ul>
+                                        </>
+                                      )
+                                }
+                                <div className="card-buttons">
+                                {expandedRestaurant !== restaurant && (
+                                    <>
+                                    <button onClick={() => setExpandedRestaurant(restaurant)}>
+                                        Horário de funcionamento
+                                    </button>
+                                    <a
+                                        target="_blank"
+                                        href={`https://wa.me/55${restaurant.cellPhone}?text=Ol%C3%A1,%20Olá%20gostaria%20de%20fazer%20o%20uma%20reserva`}
+                                        rel="noreferrer"
+                                    >Entrar em contato</a>
+                                    </>
+                                )}
+                                </div>
+                            </li>
+                        )) : (
+                            <li>Ainda não há restaurantes cadastrados</li>
+                        )
+                    }
+                </ul>
+            </div>
+        </HomeStyle>
+        </>
+    )
+}
---
+++ frontend/src/pages/isOpenPage/index.tsx
@@ -0,0 +1,66 @@
+import { useForm } from "react-hook-form"
+import { Header } from "../../components/header"
+import { useRestaurantContext } from "../../contexts/restaurantsContext"
+import cover from "../../assets/isopencover.jpeg"
+import { IsOpenPageStyle } from "./style"
+import { useNavigate } from "react-router"
+import React from "react"
+
+
+export const IsOpenPage = () => {
+    const { statusRestaurant, isOpenRestaurants, weeksDay, restaurants } = useRestaurantContext()
+
+  const navigate = useNavigate()
+    
+    const {
+        register, 
+        handleSubmit,
+        formState: {errors},
+    } = useForm()
+
+    const submit = async (data: any) => {
+        isOpenRestaurants(data)
+        console.log(statusRestaurant)
+    }
+
+    return (
+        <>
+        <Header/>
+        <IsOpenPageStyle>
+            <form onSubmit={handleSubmit(submit)}>
+                <div className="head-container">
+                    <label htmlFor="">Nome</label>
+                    <button onClick={() => {navigate("/")}}>Voltar</button>
+                </div>
+                    <select {...register("name")}>
+                        {
+                            restaurants.map((rest:any, index:number) => (
+                                <option key={index} value={rest.name}>{rest.name}</option>
+                            ))
+                        }
+                    </select>
+                <label htmlFor="">Dia da semana</label>
+                    <select {...register(`dayOfWeek`)}>
+                        {
+                            weeksDay.map((day:string, index: number) => (
+                                <option key={index} value={day}>{day}</option>
+                            ))
+                        }
+                    </select> 
+                <label htmlFor="">Horário</label>
+                <input
+                    type="time"
+                    {...register(`time`)}
+                />
+                <button id="create" type="submit">
+                    Verificar disponibilidade de Restaurante
+                </button>
+            </form>
+            <img className="cover" src={cover} alt=""/>
+            <div className="answer">
+                {statusRestaurant}
+            </div>
+        </IsOpenPageStyle>
+        </>
+    )
+}
