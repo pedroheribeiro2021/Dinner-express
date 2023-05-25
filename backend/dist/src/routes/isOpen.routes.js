e5a06c83ae692553511dc13414951b249cf7b79e
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-05-25 13:15:47 -0300

    exclude dist

--- backend/dist/src/routes/isOpen.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isOpenRoutes = void 0;
-const express_1 = require("express");
-const restaurants_controller_1 = require("../controllers/restaurants/restaurants.controller");
-exports.isOpenRoutes = (0, express_1.Router)();
-exports.isOpenRoutes.post('/', restaurants_controller_1.isOpenController);
