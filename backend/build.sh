7dd27cf29de4b68d147e0849da0fd49f6c201c32
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-05-25 12:54:05 -0300

    back to yarn

--- backend/build.sh
+++ backend/build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# yarn
-# yarn build
-# yarn typeorm migration:run -d dist/src/data-source
+yarn
+yarn build
+yarn typeorm migration:run -d dist/src/data-source
 # cd backend
-npm install
-npm run build
-npm run typeorm migration:run -- -d dist/src/data-source
+# npm install
+# npm run build
+# npm run typeorm migration:run -- -d dist/src/data-source
